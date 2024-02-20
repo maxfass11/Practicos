52ac9b63bf6a8b3ff85c168169f05a3dad8f3dc0
maxfass11 <102393232+maxfass11@users.noreply.github.com>
2024-02-20 20:31:09 -0300

    TP JSA2

---
+++ TP JS A/frases.js
@@ -0,0 +1,21 @@
+var frases = [
+"No hay hombre tan cobarde a quien el amor no haga valiente y transforme en héroe. Platón.",
+"Hay alguien tan inteligente que aprende de la experiencia de los demás. Voltaire.",
+"No puedo enseñar nada a nadie. Solo puedo hacerles pensar. Sócrates.",
+"La felicidad de tu vida depende de la calidad de tus pensamientos. Marco Aurelio. ",
+"La vida no se trata de encontrarte a ti mismo, sino de crearte a ti mismo. Bernard Shaw. ",
+"Pensar es fácil, actuar es difícil, y poner los pensamientos de uno mismo en acción es lo más difícil del mundo. Goethe.",
+"Felicidad no es hacer lo que uno quiere sino querer lo que uno hace. Jean Paul Sartre.",
+"El hombre que mueve montañas empieza apartando piedras pequeñas. Confucio.",
+"Educa a los niños y no será necesario castigar a los hombres. Pitágoras de Samos. ",
+"La verdadera libertad consiste en el dominio absoluto de sí mismo. Galileo Galilei.",
+"Los hombres geniales empiezan grandes obras, los hombres trabajadores las terminan. Leonardo Da Vinci.",
+"La buena conciencia es la mejor almohada para dormir. Sócrates.",
+"Todo el mundo ve lo que aparentas ser, pocos experimentan lo que realmente eres. Maquiavelo.",
+"Iré a cualquier parte, siempre que sea hacia adelante. David Livingstone. ",
+"Al final no son los años en nuestra vida lo que cuenta, sino la vida en nuestros años. Abraham Lincoln. ",
+"La vida debe ser comprendida hacia atrás. Pero debe ser vivida hacia delante. Kierkegaard.",
+"No malgastes tu tiempo, pues de esa materia está formada la vida. Benjamin Franklin. "
+];
+//Frases extraidas de 30 frases célebres que te harán reflexionar
+//https://www.lavanguardia.com/vivo/psicologia/20220326/8154292/30-frases-celebres-te-haran-reflexionar-nbs.html
